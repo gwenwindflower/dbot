0562d0e5fffb15745135a82d29fdb301786f718c
gwen windflower <gwenwindflower@gmail.com>
2023-04-06 00:07:00 -0500

    Add root endpoint

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ from question_answerer import QuestionAnswerer
 qa = QuestionAnswerer()
 app = FastAPI()
 
+@app.get("/")
+def hello():
+    return "Welcome, I'm dbt's question answering bot."
 
 @app.get("/answer")
 def get_answer(question: str = "What is dbt?"):
     return {"answer": qa.answer_question(question)}
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
